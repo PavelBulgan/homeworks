3acbe6f98f95ee8cfe39900f61a0e57eb8168033
PavelBulgan <pbregis@yandex.ru>
2024-08-01 22:43:46 +0300

    Add files via upload

---
+++ module_1_4.py'.py
@@ -0,0 +1,9 @@
+my_string = input('Введите текст: ')
+print(my_string)
+print('Количество введенных символов: ', len(my_string))
+print('Верхний регистр: ', my_string.upper())
+print('Нижний регистр: ', my_string.lower())
+print('Без пробелов: ', my_string.replace(' ', ''))
+print('Первый символ: ', my_string[0])
+print('Последний символ: ', my_string[-1])
+
