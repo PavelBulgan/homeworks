e780cf5091a46430ec7b89c98a83a14c3afc549e
PavelBulgan <pbregis@yandex.ru>
2024-08-01 22:17:24 +0300

    Add files via upload

---
+++ module_1_3.py.py
@@ -0,0 +1,16 @@
+name = ('Pavel')
+print('Name: ' + name)
+age = 40
+age = (str(age))
+print('Age: ' + age)
+age = (int(age))
+age = age + 1
+age = (str(age))
+print('Age after 1 year: ' + age)
+is_student = True
+is_student = (str(is_student))
+print('Is Student: ' + is_student)
+
+
+
+
