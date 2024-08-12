bad15aaf6dedae3852026470b9b44b8ae4eb43a3
PavelBulgan <pbregis@yandex.ru>
2024-08-12 23:21:40 +0300

    Delete операторы.py

--- операторы.py
+++
@@ -1,9 +0,0 @@
-first = int(input('Первое число: '))
-second = int(input('Второе число: '))
-third = int(input('Третье число: '))
-if first == second == third:
-    print('все числа равны друг другу')
-elif first == second or second == third or first == third:
-    print('2 числа равны')
-else:
-    print('нет равных чисел')
