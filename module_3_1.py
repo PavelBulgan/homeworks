662a6a2059fee7e4ea256131310f83c895ac3bf0
PavelBulgan <pbregis@yandex.ru>
2024-08-28 20:48:06 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,21 @@
+calls = 0
+def count_calls():
+    global calls
+    calls = calls + 1
+def string_info(name):
+    count_calls()
+    li = (len(name), name.upper(), name.lower())
+    return li
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        if i.lower() == string.lower():
+            return True
+    return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
